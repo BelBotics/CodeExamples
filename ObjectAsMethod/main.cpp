ec87b03785c7ac21316c2c1798cd99cb9e255a99
Pavle Ilic <pppavloti@gmail.com>
2025-04-25 12:15:45 +0200

    ADD example for sending parameters as class objects

---
+++ ObjectAsMethod/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+
+// Create basic class which will hold parameters of configuration
+// some integer, double and string values
+
+class BMSParameters
+{
+    public:
+        BMSParameters()
+        {
+            std::cout << "BMSParameters constructor called" << std::endl;
+        };
+        ~BMSParameters()
+        {
+            std::cout << "BMSParameters destructor called" << std::endl;
+        };
+        int m_numOfCells;
+        double m_current;
+        std::string m_cellManufacturer; 
+};
+
+class ReceiverClass
+{
+    public:
+        void PrintParams(BMSParameters * bmsParams)
+        {        
+            std::cout << "Printing params: " << bmsParams->m_cellManufacturer << std::endl;
+        }
+};
+
+class Parent
+{
+    public:
+        Parent()
+        {
+            std::cout << "Parent class constructor called" << std::endl;
+
+            m_bmsParams.m_cellManufacturer = "MX553BUH82";
+        };
+        ~Parent()
+        {
+            std::cout << "Parent class destructor called" << std::endl;
+        }
+
+        void TransferData()
+        {
+            m_receiver.PrintParams(&m_bmsParams);
+        }
+    private:
+        BMSParameters m_bmsParams;
+        ReceiverClass m_receiver;
+};
+
+
+
+int main()
+{
+
+    std::cout << "Hello world" << std::endl;
+
+    // create an object of class Parent
+    Parent myParentObj;
+
+    myParentObj.TransferData();
+
+
+    return 0;
+}
