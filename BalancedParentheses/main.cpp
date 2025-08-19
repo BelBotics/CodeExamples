e6760feeebb7aeb5822b430a8b48d5af5c1d86cf
pppavloti <pppavloti@gmail.com>
2025-08-19 13:42:51 +0200

    ADD user input i the loop and exit on entering q

--- BalancedParentheses/main.cpp
+++ BalancedParentheses/main.cpp
@@ -9,42 +9,64 @@
 
 using namespace std;
 
-std::string inputString = "(()a+b)*(c+d))(";
-std::stack<char> myStack;
+std::string inputString;
 
 int main()
 {
-    std::cout<< "Hello world" << endl;
+    std::cout<< "Find if parathensis ballanced or not. (Press q to exit)" << endl;
 
-    for(auto it = inputString.begin(); it<inputString.end(); it++)
+    while(1)
     {
-        if(*it == '(')
+        // crate stack object
+        std::stack<char> myStack;
+
+        // clear input string
+        inputString.clear();
+
+        std::cout << "Enter string:" << std::endl;
+
+        // get input string from console
+        std::cin >> inputString;
+
+        if(*inputString.begin() == 'q' && inputString.length() == 1)
         {
-            myStack.push(*it);
+            // user entered q to terminate the SW execution
+            return 0;
         }
-        else if(*it == ')')
+
+        for(auto it = inputString.begin(); it<inputString.end(); it++)
         {
-            // pop if not empty
-            if(myStack.empty() == false)
+            if(*it == '(')
             {
-                myStack.pop();
+                myStack.push(*it);
             }
-            else
+            else if(*it == ')')
             {
-                std::cout << "Parenthesis not balanced" << std::endl;
-                return 0;
+                // pop if not empty
+                if(myStack.empty() == false)
+                {
+                    myStack.pop();
+                }
+                else
+                {
+                    std::cout << "Parenthesis NOT balanced" << std::endl;
+                    goto label1;
+                }
             }
         }
+        
+        if(myStack.empty() == true)
+        {
+            std::cout << "Parenthesis balanced" << std::endl;
+        }
+        else
+        {
+            std::cout << "Parenthesis NOT balanced" << std::endl;
+        }
+
+        label1:
+        std::cout << "----------------------" << std::endl;
     }
-    if(myStack.empty() == true)
-    {
-        std::cout << "Parenthesis balanced" << std::endl;
-    }
-    else
-    {
-        std::cout << "Parenthesis NOT balanced" << std::endl;
-    }
-    
     
     // add comment to set up gitHub Account
     return 0;
